03d16e5b4b8d700603cd89fbdb26b17223823870
Jakub Borys <jakub.borys@gmail.com>
2015-11-08 14:54:42 +0000

    PLAT-48 100% Test coverage.

--- nameko_entrypoint_logger.py
+++ nameko_entrypoint_logger.py
@@ -2,18 +2,15 @@ import inspect
 import json
 import logging
 import socket
-
 from datetime import datetime
 from traceback import format_tb
 from weakref import WeakKeyDictionary
-
 from kombu import Connection
 from kombu.pools import connections, producers
 from nameko.constants import (
     DEFAULT_RETRY_POLICY, DEFAULT_SERIALIZER,
     SERIALIZER_CONFIG_KEY
 )
-
 from nameko.events import EventHandler
 from nameko.extensions import DependencyProvider
 from nameko.messaging import AMQP_URI_CONFIG_KEY
@@ -23,93 +20,6 @@ from nameko.utils import get_redacted_args
 from nameko.web.handlers import HttpRequestHandler
 
 
-def default(obj):
-    """Default JSON serializer."""
-    if isinstance(obj, datetime):
-        return obj.isoformat()
-    raise ValueError
-
-
-def dumps(obj):
-    return json.dumps(obj, default=default)
-
-
-def event_dispatcher(nameko_config, exchange_name, routing_key):
-    """ Return a function that dispatches nameko events.
-    """
-    def dispatch(event_data):
-        """ Dispatch an event claiming to originate from `service_name` with
-        the given `event_type` and `event_data`.
-        """
-        conn = Connection(nameko_config[AMQP_URI_CONFIG_KEY])
-
-        serializer = nameko_config.get(
-            SERIALIZER_CONFIG_KEY, DEFAULT_SERIALIZER)
-
-        exchange = get_event_exchange(exchange_name)
-
-        with connections[conn].acquire(block=True) as connection:
-            exchange.maybe_bind(connection)
-            with producers[conn].acquire(block=True) as producer:
-                msg = event_data
-                producer.publish(
-                    msg,
-                    exchange=exchange,
-                    declare=[exchange],
-                    serializer=serializer,
-                    routing_key=routing_key,
-                    retry=True,
-                    retry_policy=DEFAULT_RETRY_POLICY
-                )
-    return dispatch
-
-
-def parse_request(request):
-
-    return {
-        'content_type': request.content_type,
-        'url': request.url,
-        'cookies': request.cookies,
-        'method': request.method
-    }
-
-
-def get_worker_data(worker_ctx):
-
-    provider = worker_ctx.entrypoint
-    service_name = worker_ctx.service_name
-    provider_name = provider.method_name
-    entrypoint = "{}.{}".format(service_name, provider_name)
-
-    if hasattr(provider, 'sensitive_variables'):
-        redacted_callargs = get_redacted_args(
-            provider, *worker_ctx.args, **worker_ctx.kwargs)
-    else:
-        method = getattr(provider.container.service_cls, provider.method_name)
-        callargs = inspect.getcallargs(method, None, *worker_ctx.args, **worker_ctx.kwargs)
-        del callargs['self']
-        if 'request' in callargs:
-            callargs['request'] = parse_request(callargs['request'])
-        redacted_callargs = callargs
-
-    return {
-        'timestamp': datetime.utcnow(),
-        'provider': type(provider).__name__,
-        'hostname': socket.gethostname(),
-        'service': service_name,
-        'provider_name': provider_name,
-        'entrypoint': entrypoint,
-        'call_id': worker_ctx.call_id,
-        'call_stack': worker_ctx.call_id_stack,
-        'context_data': {
-            'language': worker_ctx.data.get('language'),
-            'user_id': worker_ctx.data.get('user_id'),
-            'user_agent': worker_ctx.data.get('user_agent'),
-        },
-        'call_args': redacted_callargs
-    }
-
-
 class BroadcastLogHandler(logging.Handler):
     # Broadcast log messages to RabbitMq queue
     # TODO: Pre-queue in memory with eventlet.queue
@@ -117,14 +27,12 @@ class BroadcastLogHandler(logging.Handler):
         self,
         dispatcher
     ):
+
         self.dispatcher = dispatcher
         logging.Handler.__init__(self)
 
     def emit(self, message):
         self.dispatcher(message.getMessage())
-
-    def flush(self):
-        pass
 
 
 class EntrypointLogger(DependencyProvider):
@@ -133,6 +41,7 @@ class EntrypointLogger(DependencyProvider):
         self.routing_key = routing_key
         self.propagate = propagate
         self.logger = None
+        self.worker_timestamps = WeakKeyDictionary()
 
     entrypoint_types = (Rpc, EventHandler, HttpRequestHandler)
 
@@ -153,7 +62,6 @@ class EntrypointLogger(DependencyProvider):
         logger.addHandler(handler)
 
         self.logger = logger
-        self.worker_timestamps = WeakKeyDictionary()
 
     def worker_setup(self, worker_ctx):
 
@@ -218,3 +126,96 @@ class EntrypointLogger(DependencyProvider):
                 'expected_error': is_expected,
             })
         self.logger.info(dumps(data))
+
+
+def default(obj):
+    """Default JSON serializer.
+    :param obj: datetime
+    """
+    if isinstance(obj, datetime):
+        return obj.isoformat()
+    raise ValueError
+
+
+def dumps(obj):
+    return json.dumps(obj, default=default)
+
+
+def event_dispatcher(nameko_config, exchange_name, routing_key):
+    """ Return a function that dispatches nameko events.
+    :param nameko_config: nameko configuration
+    :param exchange_name: exchange where events should be published to
+    :param routing_key: event routing key
+    """
+
+    def dispatch(event_data):
+        """ Dispatch an event
+        :param event_data: event payload
+        """
+        conn = Connection(nameko_config[AMQP_URI_CONFIG_KEY])
+
+        serializer = nameko_config.get(
+            SERIALIZER_CONFIG_KEY, DEFAULT_SERIALIZER)
+
+        exchange = get_event_exchange(exchange_name)
+
+        with connections[conn].acquire(block=True) as connection:
+            exchange.maybe_bind(connection)
+            with producers[conn].acquire(block=True) as producer:
+                msg = event_data
+                producer.publish(
+                    msg,
+                    exchange=exchange,
+                    declare=[exchange],
+                    serializer=serializer,
+                    routing_key=routing_key,
+                    retry=True,
+                    retry_policy=DEFAULT_RETRY_POLICY
+                )
+
+    return dispatch
+
+
+def get_worker_data(worker_ctx):
+    provider = worker_ctx.entrypoint
+    service_name = worker_ctx.service_name
+    provider_name = provider.method_name
+    entrypoint = "{}.{}".format(service_name, provider_name)
+
+    if hasattr(provider, 'sensitive_variables'):
+        redacted_callargs = get_redacted_args(
+            provider, *worker_ctx.args, **worker_ctx.kwargs)
+    else:
+        method = getattr(provider.container.service_cls, provider.method_name)
+        callargs = inspect.getcallargs(method, None, *worker_ctx.args,
+                                       **worker_ctx.kwargs)
+        del callargs['self']
+        if 'request' in callargs:
+            callargs['request'] = parse_request(callargs['request'])
+        redacted_callargs = callargs
+
+    return {
+        'timestamp': datetime.utcnow(),
+        'provider': type(provider).__name__,
+        'hostname': socket.gethostname(),
+        'service': service_name,
+        'provider_name': provider_name,
+        'entrypoint': entrypoint,
+        'call_id': worker_ctx.call_id,
+        'call_stack': worker_ctx.call_id_stack,
+        'context_data': {
+            'language': worker_ctx.data.get('language'),
+            'user_id': worker_ctx.data.get('user_id'),
+            'user_agent': worker_ctx.data.get('user_agent'),
+        },
+        'call_args': redacted_callargs
+    }
+
+
+def parse_request(request):
+    return {
+        'content_type': request.content_type,
+        'url': request.url,
+        'cookies': request.cookies,
+        'method': request.method
+    }
--- test_nameko_entrypoint_logger.py
+++ test_nameko_entrypoint_logger.py
@@ -1,21 +1,24 @@
-import logging
 import json
-
-from mock import Mock, patch, call
-import pytest
+import logging
 from datetime import datetime
-from nameko.containers import WorkerContext
-from nameko.rpc import rpc, Rpc
-from nameko.testing.services import dummy, entrypoint_hook, entrypoint_waiter
-from nameko.testing.utils import get_extension
-from nameko.events import EventHandler, event_handler
 
-from nameko_entrypoint_logger import BroadcastLogHandler, EntrypointLogger
+import pytest
+from mock import MagicMock, Mock, patch
+from nameko.containers import WorkerContext
+from nameko.events import EventHandler, event_handler
+from nameko.rpc import Rpc, rpc
+from nameko.testing.utils import DummyProvider, get_extension
+from nameko.web.handlers import HttpRequestHandler, http
+from werkzeug.test import create_environ
+from werkzeug.wrappers import Request
+
+from nameko_entrypoint_logger import (
+    BroadcastLogHandler, EntrypointLogger, event_dispatcher)
 
 EXCHANGE_NAME = "logging_exchange"
 ROUTING_KEY = "monitoring_event"
 
-dispatcher = Mock()
+dispatcher = MagicMock()
 
 
 class Service(object):
@@ -27,10 +30,14 @@ class Service(object):
     def rpc_method(self, foo):
         pass
 
-    @event_handler("service_b", "performance_test_request")
+    @http('GET', '/get/<int:value>')
+    def get_method(self, request, value):
+        payload = {'value': value}
+        return json.dumps(payload)
+
+    @event_handler("publisher", "routing_key")
     def handle_event(self, payload):
         self.dispatch({'success': True})
-
 
 
 @pytest.fixture
@@ -40,14 +47,44 @@ def container(container_factory):
 
 @pytest.fixture
 def entrypoint_logger(container):
-    return EntrypointLogger(
+    logger = EntrypointLogger(
         EXCHANGE_NAME, ROUTING_KEY
     ).bind(container, "service")
 
+    logger.setup()
+
+    return logger
+
+
+@pytest.fixture
+def rpc_worker_ctx(entrypoint_logger):
+    entrypoint = get_extension(
+        entrypoint_logger.container, Rpc, method_name="rpc_method"
+    )
+
+    return WorkerContext(
+        entrypoint_logger.container, Service, entrypoint, args=("bar",)
+    )
+
+
+@pytest.fixture
+def http_worker_ctx(entrypoint_logger):
+    entrypoint = get_extension(
+        entrypoint_logger.container,
+        HttpRequestHandler,
+        method_name="get_method"
+    )
+
+    environ = create_environ('/get/1', 'http://localhost:8080/')
+
+    request = Request(environ)
+
+    return WorkerContext(
+        entrypoint_logger.container, Service, entrypoint, args=(request, 1)
+    )
+
 
 def test_setup(entrypoint_logger):
-    entrypoint_logger.setup()
-
     assert BroadcastLogHandler in [
         type(handler) for handler in entrypoint_logger.logger.handlers
         if type(handler) == BroadcastLogHandler]
@@ -56,22 +93,35 @@ def test_setup(entrypoint_logger):
     assert entrypoint_logger.routing_key == ROUTING_KEY
 
 
-def test_rpc_request_is_logged(entrypoint_logger):
-
-    entrypoint = get_extension(
-        entrypoint_logger.container, Rpc, method_name="rpc_method"
-    )
-
-    args = ("bar",)
-
-    worker_ctx = WorkerContext(
-        entrypoint_logger.container, Service, entrypoint, args=args
-    )
-
-    entrypoint_logger.setup()
+def test_will_only_process_request_from_known_entrypoints(
+    entrypoint_logger,
+    mock_container
+):
+    mock_container.service_name = "service"
+    worker_ctx = WorkerContext(mock_container, None, DummyProvider())
 
     with patch.object(entrypoint_logger, 'logger') as logger:
         entrypoint_logger.worker_setup(worker_ctx)
+
+    assert not logger.info.called
+
+
+def test_will_only_process_results_from_known_entrypoints(
+    entrypoint_logger,
+    mock_container
+):
+    mock_container.service_name = "service"
+    worker_ctx = WorkerContext(mock_container, None, DummyProvider())
+
+    with patch.object(entrypoint_logger, 'logger') as logger:
+        entrypoint_logger.worker_result(worker_ctx)
+
+    assert not logger.info.called
+
+
+def test_rpc_request_is_logged(entrypoint_logger, rpc_worker_ctx):
+    with patch.object(entrypoint_logger, 'logger') as logger:
+        entrypoint_logger.worker_setup(rpc_worker_ctx)
 
     (call_args,), _ = logger.info.call_args
 
@@ -81,24 +131,11 @@ def test_rpc_request_is_logged(entrypoint_logger):
     assert worker_data['call_args'] == {"foo": "bar"}
 
 
-def test_rpc_response_is_logged(entrypoint_logger):
-
-    entrypoint = get_extension(
-        entrypoint_logger.container, Rpc, method_name="rpc_method"
-    )
-
-    args = ("bar",)
-
-    worker_ctx = WorkerContext(
-        entrypoint_logger.container, Service, entrypoint, args=args
-    )
-
-    entrypoint_logger.setup()
-
-    entrypoint_logger.worker_timestamps[worker_ctx] = datetime.utcnow()
+def test_rpc_response_is_logged(entrypoint_logger, rpc_worker_ctx):
+    entrypoint_logger.worker_timestamps[rpc_worker_ctx] = datetime.utcnow()
 
     with patch.object(entrypoint_logger, 'logger') as logger:
-        entrypoint_logger.worker_result(worker_ctx, result={'bar': 'foo'})
+        entrypoint_logger.worker_result(rpc_worker_ctx, result={'bar': 'foo'})
 
     (call_args,), _ = logger.info.call_args
 
@@ -109,26 +146,15 @@ def test_rpc_response_is_logged(entrypoint_logger):
     assert worker_data['result'] == '{"bar": "foo"}'
 
 
-def test_rpc_unexpected_exception_is_logged(entrypoint_logger):
-    entrypoint = get_extension(
-        entrypoint_logger.container, Rpc, method_name="rpc_method"
-    )
-
-    args = ("bar",)
+def test_rpc_unexpected_exception_is_logged(entrypoint_logger, rpc_worker_ctx):
     exception = Exception("Something went wrong")
     exc_info = (Exception, exception, exception.__traceback__)
 
-    worker_ctx = WorkerContext(
-        entrypoint_logger.container, Service, entrypoint, args=args
-    )
-
-    entrypoint_logger.setup()
-
-    entrypoint_logger.worker_timestamps[worker_ctx] = datetime.utcnow()
+    entrypoint_logger.worker_timestamps[rpc_worker_ctx] = datetime.utcnow()
 
     with patch.object(entrypoint_logger, 'logger') as logger:
         entrypoint_logger.worker_result(
-            worker_ctx, result={'bar': 'foo'}, exc_info=exc_info
+            rpc_worker_ctx, result={'bar': 'foo'}, exc_info=exc_info
         )
 
     (call_args,), _ = logger.info.call_args
@@ -141,26 +167,15 @@ def test_rpc_unexpected_exception_is_logged(entrypoint_logger):
     assert "Something went wrong" in str(worker_data['exc'])
 
 
-def test_rpc_expected_exception_is_logged(entrypoint_logger):
-    entrypoint = get_extension(
-        entrypoint_logger.container, Rpc, method_name="rpc_method"
-    )
-
-    args = ("bar",)
+def test_rpc_expected_exception_is_logged(entrypoint_logger, rpc_worker_ctx):
     exception = ValueError("Invalid value")
     exc_info = (Exception, exception, exception.__traceback__)
 
-    worker_ctx = WorkerContext(
-        entrypoint_logger.container, Service, entrypoint, args=args
-    )
-
-    entrypoint_logger.setup()
-
-    entrypoint_logger.worker_timestamps[worker_ctx] = datetime.utcnow()
+    entrypoint_logger.worker_timestamps[rpc_worker_ctx] = datetime.utcnow()
 
     with patch.object(entrypoint_logger, 'logger') as logger:
         entrypoint_logger.worker_result(
-            worker_ctx, result={'bar': 'foo'}, exc_info=exc_info
+            rpc_worker_ctx, result={'bar': 'foo'}, exc_info=exc_info
         )
 
     (call_args,), _ = logger.info.call_args
@@ -173,6 +188,75 @@ def test_rpc_expected_exception_is_logged(entrypoint_logger):
     assert "Invalid value" in str(worker_data['exc'])
 
 
+def test_can_fail_exception_repr(entrypoint_logger, rpc_worker_ctx):
+    exception = ValueError("Invalid value")
+    mock_exception = Mock()
+    mock_exception.__repr__ = lambda s: (_ for _ in ()).throw(Exception())
+    exc_info = (Exception, mock_exception, exception.__traceback__)
+
+    entrypoint_logger.worker_timestamps[rpc_worker_ctx] = datetime.utcnow()
+
+    with patch.object(entrypoint_logger, 'logger') as logger:
+        entrypoint_logger.worker_result(
+            rpc_worker_ctx, result={'bar': 'foo'}, exc_info=exc_info
+        )
+
+    (call_args,), _ = logger.info.call_args
+
+    worker_data = json.loads(call_args)
+
+    assert worker_data['exc'] == '[exc __repr__ failed]'
+
+
+def test_http_request_is_logged(entrypoint_logger, http_worker_ctx):
+    with patch.object(entrypoint_logger, 'logger') as logger:
+        entrypoint_logger.worker_setup(http_worker_ctx)
+
+    (call_args,), _ = logger.info.call_args
+
+    worker_data = json.loads(call_args)
+
+    assert worker_data['provider'] == "HttpRequestHandler"
+    assert worker_data['entrypoint'] == 'service.get_method'
+    assert worker_data['call_args'] == {
+        'request':
+            {
+                'method': 'GET',
+                'content_type': '',
+                'cookies': {},
+                'url': 'http://localhost:8080/get/1'
+            },
+        'value': 1
+    }
+
+
+def test_http_response_is_logged(entrypoint_logger, http_worker_ctx):
+    entrypoint_logger.worker_timestamps[http_worker_ctx] = datetime.utcnow()
+
+    with patch.object(entrypoint_logger, 'logger') as logger:
+        entrypoint_logger.worker_result(http_worker_ctx, result={'value': 1})
+
+    (call_args,), _ = logger.info.call_args
+
+    worker_data = json.loads(call_args)
+
+    assert worker_data['provider'] == "HttpRequestHandler"
+    assert json.loads(worker_data['result']) == {'value': 1}
+
+
+def test_can_handle_non_json_results(entrypoint_logger, http_worker_ctx):
+    entrypoint_logger.worker_timestamps[http_worker_ctx] = datetime.utcnow()
+
+    with patch.object(entrypoint_logger, 'logger') as logger:
+        entrypoint_logger.worker_result(http_worker_ctx, result={None})
+
+    (call_args,), _ = logger.info.call_args
+
+    worker_data = json.loads(call_args)
+
+    assert worker_data['result'] == "[json dump failed]"
+
+
 def test_event_handler_request_response_is_logged():
     pass
 
@@ -181,12 +265,42 @@ def test_event_handler_exception_is_logged():
     pass
 
 
-def test_http_request_response_is_logged():
-    pass
+def test_can_log_broadcast_events():
+    logger = logging.getLogger('test')
+    handler = BroadcastLogHandler(dispatcher)
+    logger.addHandler(handler)
+    message = {'foo': 'bar'}
+    logger.info(json.dumps(message))
+
+    (call_args,), _ = dispatcher.call_args
+
+    assert dispatcher.called
+    assert json.loads(call_args) == message
 
 
-def test_http_exception_is_logged():
-    pass
+def test_will_dispatch_events():
+    from nameko.constants import (
+        AMQP_URI_CONFIG_KEY, SERIALIZER_CONFIG_KEY, DEFAULT_SERIALIZER)
+
+    config = {
+        SERIALIZER_CONFIG_KEY: DEFAULT_SERIALIZER,
+        AMQP_URI_CONFIG_KEY: 'memory://'
+    }
+
+    dispatcher = event_dispatcher(config, EXCHANGE_NAME, ROUTING_KEY)
+
+    from mock import ANY
+
+    message = {'foo': 'bar'}
+
+    with patch('nameko_entrypoint_logger.producers') as mock_producers:
+        with mock_producers[ANY].acquire(block=True) as mock_producer:
+            dispatcher(json.dumps(message))
+
+    (msg,), config = mock_producer.publish.call_args
+
+    assert json.loads(msg) == message
+    assert config['routing_key'] == ROUTING_KEY
 
 
 def test_end_to_end():
