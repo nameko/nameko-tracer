e4cd714658a44f4b2606bb78fddfd1f16743bef6
timbu <tim.buckland@student.com>
2017-05-25 17:11:57 +0100

    put test back

--- test_nameko_entrypoint_logger.py
+++ test_nameko_entrypoint_logger.py
@@ -5,7 +5,7 @@ import socket
 from datetime import datetime
 
 import pytest
-# import six
+import six
 from kombu import Exchange, Queue
 from mock import ANY, call, MagicMock, Mock, patch
 from nameko.constants import AMQP_URI_CONFIG_KEY
@@ -575,38 +575,6 @@ def test_unexpected_exception_is_logged(entrypoint_logger, rpc_worker_ctx):
     assert "Something went wrong" in str(worker_data['exception']['exc'])
 
 
-# def test_unexpected_exception_with_cause_is_logged(
-#     entrypoint_logger, rpc_worker_ctx
-# ):
-#     try:
-#         cause = ValueError('This is the cause.')
-#         wrapping_error = Exception("Something went wrong")
-#         six.raise_from(wrapping_error, cause)
-#     except Exception as e:
-#         exc = e
-
-#     exc_info = (Exception, exc, exc.__traceback__)
-
-#     entrypoint_logger.worker_timestamps[rpc_worker_ctx] = datetime.utcnow()
-
-#     with patch.object(entrypoint_logger, 'logger') as logger:
-#         entrypoint_logger.worker_result(
-#             rpc_worker_ctx, result={'bar': 'foo'}, exc_info=exc_info
-#         )
-
-#     (call_args,), _ = logger.info.call_args
-
-#     worker_data = json.loads(call_args)
-
-#     assert worker_data['provider'] == "Rpc"
-#     assert worker_data['exception']['expected_error'] is False
-#     assert worker_data['status'] == 'error'
-#     assert "Something went wrong" in str(worker_data['exception']['exc'])
-#     assert "Something went wrong" in worker_data['exception']['traceback']
-#     assert "This is the cause" in worker_data['exception']['traceback']
-#     assert "ValueError" in worker_data['exception']['traceback']
-
-
 def test_expected_exception_is_logged(entrypoint_logger, rpc_worker_ctx):
     exception = CustomException("Invalid value")
     exc_info = (CustomException, exception, exception.__traceback__)
@@ -1092,3 +1060,33 @@ def test_can_handle_exception_when_getting_worker_data(entrypoint_logger):
         data = entrypoint_logger._get_base_worker_data(worker_ctx)
 
     assert error_message in data['error']
+
+
+def test_exception_with_cause_is_logged(
+    entrypoint_logger, rpc_worker_ctx
+):
+    try:
+        cause = ValueError('This is the cause.')
+        wrapping_error = Exception("Something went wrong")
+        six.raise_from(wrapping_error, cause)
+    except Exception as e:
+        exc = e
+
+    exc_info = (Exception, exc, exc.__traceback__)
+
+    entrypoint_logger.worker_timestamps[rpc_worker_ctx] = datetime.utcnow()
+
+    with patch.object(entrypoint_logger, 'logger') as logger:
+        entrypoint_logger.worker_result(
+            rpc_worker_ctx, result={'bar': 'foo'}, exc_info=exc_info
+        )
+
+    (call_args,), _ = logger.info.call_args
+
+    worker_data = json.loads(call_args)
+
+    assert worker_data['status'] == 'error'
+    assert "Something went wrong" in str(worker_data['exception']['exc'])
+    assert "Something went wrong" in worker_data['exception']['traceback']
+    assert "This is the cause" in worker_data['exception']['traceback']
+    assert "ValueError" in worker_data['exception']['traceback']
