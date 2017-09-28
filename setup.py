c2effedfb00b54597ca7a35c524ba67421a86311
Ondrej Kohout <ondrej.kohout@gmail.com>
2017-09-28 16:28:02 +0100

    Bump-up version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='nameko-tracer',
-    version='1.0.5',
+    version='1.0.6',
     description='Nameko extension logging entrypoint processing metrics',
     author='student.com',
     author_email='wearehiring@student.com',
